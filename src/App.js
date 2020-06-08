cb474cab9d875c1a1c93b46a02fe33199a4d0c36
emilygracekz <57361805+emilygracekz@users.noreply.github.com>
2020-06-08 05:46:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from 'react';
+import './App.css';
 
-const App = () => {
-  return React.createElement("div", { id: "something-cool" }, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
 
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Stray",
-    }),
-  ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Adopt Me!</h1>
+    </div>
+  );
+}
+
+export default App;
